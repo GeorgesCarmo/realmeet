ac27346d7cdf294469ba7b0e4d46053370727c6e
georges <georgesdc13@gmail.com>
2024-06-19 15:30:33 -0300

    Config inicial para a pipeline

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -2,6 +2,8 @@ package br.com.sw2you.realmeet;
 
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
 import br.com.sw2you.realmeet.api.model.Room;
+import java.util.concurrent.CompletableFuture;
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +12,7 @@ import java.util.concurrent.CompletableFuture;
 public class RoomController implements RoomsApi {
 
     @Override
-    public CompletableFuture<ResponseEntity<Room>> listRooms(Long id) {
+    public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
         return CompletableFuture.supplyAsync(()-> ResponseEntity.ok(new Room().id(1L).name("Room #1")));
     }
 }
